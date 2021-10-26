0eb513d0b799acde828f7b2211388df3bd123e97
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-10-25 18:07:45 -0700

    got my links to pop up on my map page

--- map/map.js
+++ map/map.js
@@ -1,10 +1,11 @@
-import quests from '../data/quest-data';
+import quests from '../data/quest-data.js';
 
 const mapLinks = document.getElementById('map-links');
+console.log(mapLinks);
 
 for (let quest of quests){
     const a = document.createElement('a');
-    a.href = `../quest/?=${quest.id}`;
+    a.href = `../quest/?id=${quest.id}`;
     a.textContent = quest.title;
 
     mapLinks.appendChild(a);
